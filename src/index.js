d520648d3f8562cd526f33bb066e35c703eeb58c
Jon Redmond <jredmond@sapient.com>
2019-01-01 13:27:47 +0000

    complete step 01

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { hot } from 'react-hot-loader'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <Wrapper>
+        <Title>Hello World</Title>
+      </Wrapper>
+    );
   }
 }
 
 const AppWithHot = hot(module)(App);
 
-var mountNode = document.getElementById("app");
+var mountNode = document.getElementById('app');
 ReactDOM.render(<AppWithHot />, mountNode);
